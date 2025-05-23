c5c6f45055f95deadeb7b4e9946b7721b80bd499
Dwayne9078 <pabloroo87@gmail.com>
2025-05-23 15:30:09 -0600

    feature: Diseño wearable y TV finalizado

--- src/index.php
+++ src/index.php
@@ -2,18 +2,13 @@
 <html lang="es">
 
 <head>
-
   <meta charset="UTF-8" />
-
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Felpudo College</title>
-
   <link rel="stylesheet" href="./styles/index.css" />
-
   <link
     href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
     rel="stylesheet" />
-
   <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
@@ -21,104 +16,124 @@
 </head>
 
 <body>
-  <div class="desktop">
-    <div class="header"> <a href="#"> </a>
+  <div class="container">
+    <header class="wearable-header">
       <div class="logo">
-        <img
-          src="./assets/felpudo.png"
-          width="128" alt="Felpudo College Logo" />
+        <img src="./assets/felpudo.png" alt="Felpudo College Logo" class="logo-img">
       </div>
-      <i class="material-icons icon">person</i>
-    </div>
+    </header>
 
-    <div class="main-content">
-      <h1 class="section-title">Partidos</h1>
-      <div class="matches-container">
-        <div class="match-card past-match">
-          <div class="team">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Real-madrid-icon-1467x2048-p4mfm7ya.png/250px-Real-madrid-icon-1467x2048-p4mfm7ya.png"
-              alt="Real Madrid Logo" />
-            <span>Real Madrid</span>
-          </div>
-          <span class="vs">VS</span>
-          <div class="team">
-            <img src="https://upload.wikimedia.org/wikipedia/en/b/ba/ClubUniversidadNacional.PNG" alt="Pumas Logo" />
-            <span>Pumas</span>
-          </div>
-          <div class="score">3 - 9</div>
-          <div class="stadium">Felpudo Stadium</div>
+    <main class="wearable-main">
+      <div class="wearable-navigation">
+        <a href="./pages/horarios.php" class="nav-button horarios">
+          <i class="material-icons">schedule</i>
+          <span>Horarios</span>
+        </a>
+        <a href="./pages/resultados.php" class="nav-button resultados">
+          <i class="material-icons">emoji_events</i>
+          <span>Resultados</span>
+        </a>
+      </div>
+    </main>
+
+    <div class="original-view">
+      <div class="header"> <a href="#"> </a>
+        <div class="logo">
+          <img
+            src="./assets/felpudo.png"
+            width="128" alt="Felpudo College Logo" />
         </div>
+        <i class="material-icons icon">person</i>
+      </div>
 
-        <div class="carousel-wrapper">
-          <div class="match-card live-match">
+      <div class="main-content">
+        <h1 class="section-title">Partidos</h1>
+        <div class="matches-container">
+          <div class="match-card past-match">
             <div class="team">
               <img
-                src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/Guadalajara_CD.svg/800px-Guadalajara_CD.svg.png"
-                alt="Chivas Logo" />
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Real-madrid-icon-1467x2048-p4mfm7ya.png/250px-Real-madrid-icon-1467x2048-p4mfm7ya.png"
+                alt="Real Madrid Logo" />
+              <span>Real Madrid</span>
             </div>
-            <span class="vs-live">VS</span>
+            <span class="vs">VS</span>
             <div class="team">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/Club_Am%C3%A9rica_crest.svg/1200px-Club_Am%C3%A9rica_crest.svg.png"
-                alt="America Logo" />
+              <img src="https://upload.wikimedia.org/wikipedia/en/b/ba/ClubUniversidadNacional.PNG" alt="Pumas Logo" />
+              <span>Pumas</span>
             </div>
-            <div class="live-info">
-              <span class="live-dot"></span>
-              En vivo
-              <br />
-              17:30
-            </div>
+            <div class="score">3 - 9</div>
             <div class="stadium">Felpudo Stadium</div>
           </div>
+
+          <div class="carousel-wrapper">
+            <div class="match-card live-match">
+              <div class="team">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/Guadalajara_CD.svg/800px-Guadalajara_CD.svg.png"
+                  alt="Chivas Logo" />
+              </div>
+              <span class="vs-live">VS</span>
+              <div class="team">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/Club_Am%C3%A9rica_crest.svg/1200px-Club_Am%C3%A9rica_crest.svg.png"
+                  alt="America Logo" />
+              </div>
+              <div class="live-info">
+                <span class="live-dot"></span>
+                En vivo
+                <br />
+                17:30
+              </div>
+              <div class="stadium">Felpudo Stadium</div>
+            </div>
+          </div>
+
+          <div class="match-card future-match">
+            <div class="team">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Club_de_F%C3%BAtbol_Monterrey_2019_Logo.svg/800px-Club_de_F%C3%BAtbol_Monterrey_2019_Logo.svg.png"
+                alt="Monterrey Logo" />
+              <span>Monterrey</span>
+            </div>
+            <span class="vs">VS</span>
+            <div class="team">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Real-madrid-icon-1467x2048-p4mfm7ya.png/250px-Real-madrid-icon-1467x2048-p4mfm7ya.png"
+                alt="Real Madrid Logo" />
+              <span>Real Madrid</span>
+            </div>
+            <div class="time">
+              Mañana <br />
+              16:00
+            </div>
+            <div class="stadium">Estadio Central</div>
+          </div>
         </div>
 
-        <div class="match-card future-match">
-          <div class="team">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Club_de_F%C3%BAtbol_Monterrey_2019_Logo.svg/800px-Club_de_F%C3%BAtbol_Monterrey_2019_Logo.svg.png"
-              alt="Monterrey Logo" />
-            <span>Monterrey</span>
+        <div class="navigation-cards">
+          <div class="nav-card horarios">
+            <a href="./pages/horarios.php">
+              <i class="material-icons nav-icon">schedule</i> <span>Horarios</span>
+            </a>
           </div>
-          <span class="vs">VS</span>
-          <div class="team">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Real-madrid-icon-1467x2048-p4mfm7ya.png/250px-Real-madrid-icon-1467x2048-p4mfm7ya.png"
-              alt="Real Madrid Logo" />
-            <span>Real Madrid</span>
+          <div class="nav-card reserv">
+            <a href="./pages/reservar.php">
+              <i class="material-icons nav-icon">event_seat</i> <span>Reservar</span>
+            </a>
           </div>
-          <div class="time">
-            Mañana <br />
-            16:00
+          <div class="nav-card resultados">
+            <a href="./pages/resultados.php">
+              <i class="material-icons nav-icon">emoji_events</i> <span>Resultados</span>
+            </a>
           </div>
-          <div class="stadium">Estadio Central</div>
+          <div class="nav-card informacion">
+            <a href="./pages/informacion.php">
+              <i class="material-icons nav-icon">info</i> <span>Información General</span>
+            </a>
+          </div>
         </div>
+        </main>
       </div>
-
-      <div class="navigation-cards">
-        <div class="nav-card horarios">
-          <a href="./pages/horarios.php">
-            <i class="material-icons nav-icon">schedule</i> <span>Horarios</span>
-          </a>
-        </div>
-        <div class="nav-card reserv">
-          <a href="./pages/reservar.php">
-            <i class="material-icons nav-icon">event_seat</i> <span>Reservar</span>
-          </a>
-        </div>
-        <div class="nav-card resultados">
-          <a href="./pages/resultados.php">
-            <i class="material-icons nav-icon">emoji_events</i> <span>Resultados</span>
-          </a>
-        </div>
-        <div class="nav-card informacion">
-          <a href="./pages/informacion.php">
-            <i class="material-icons nav-icon">info</i> <span>Información General</span>
-          </a>
-        </div>
-      </div>
-      </main>
-
     </div>
 </body>
 
--- src/pages/resultados.php
+++ src/pages/resultados.php
@@ -5,104 +5,225 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Felpudo College</title>
+
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="../styles/resultados.css">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
 </head>
 
-<body>
-    <div class="header">
-        <a href="../index.php">
-            <i class="material-icons icon">arrow_back</i>
-        </a>
-        <div class="logo">
-            <img src="../assets/felpudo.png" alt="Logo" height="48">
+<body class="bg-[#39722d]">
+
+    <div class="normal">
+        <div class="header">
+            <a href="../index.php">
+                <i class="material-icons icon">arrow_back</i>
+            </a>
+            <div class="logo">
+                <img src="../assets/felpudo.png" alt="Logo" width="100">
+            </div>
+            <i class="material-icons icon">person</i>
         </div>
-        <i class="material-icons icon">person</i>
+
+        <main class="main-content">
+            <section class="results-date-range">
+                Resultados: Dom 11 - Sab 17 Mayo
+            </section>
+
+            <section class="match-day">
+                <h2>Lunes 12</h2>
+                <div class="match-cards-container">
+                    <div class="match-card">
+                        <div class="team">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Club_de_F%C3%BAtbol_Monterrey_2019_Logo.svg/800px-Club_de_F%C3%BAtbol_Monterrey_2019_Logo.svg.png" alt="Monterrey Logo" class="team-logo">
+                            <span class="team-name">Monterrey</span>
+                        </div>
+                        <span class="vs">vs</span>
+                        <div class="team">
+                            <img src="https://upload.wikimedia.org/wikipedia/en/5/56/Esteghlal_FC_Official_Logo.png" alt="Irán Logo" class="team-logo">
+                            <span class="team-name">Irán</span>
+                        </div>
+                        <div class="score">
+                            <span class="score-home">2</span>
+                            <span class="score-away">0</span>
+                        </div>
+                        <div class="winner">GANADOR Monterrey</div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="match-day">
+                <h2>Miercoles 14</h2>
+                <div class="match-cards-container">
+                    <div class="match-card">
+                        <div class="team">
+                            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png" alt="Manchester Logo" class="team-logo">
+                            <span class="team-name">Manchester</span>
+                        </div>
+                        <span class="vs">vs</span>
+                        <div class="team">
+                            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/Guadalajara_CD.svg/800px-Guadalajara_CD.svg.png" alt="Chivas Logo" class="team-logo">
+                            <span class="team-name">Chivas</span>
+                        </div>
+                        <div class="score">
+                            <span class="score-home">1</span>
+                            <span class="score-away">4</span>
+                        </div>
+                        <div class="winner">GANADOR Chivas</div>
+                    </div>
+
+                    <div class="match-card">
+                        <div class="team">
+                            <img src="https://upload.wikimedia.org/wikipedia/en/b/ba/ClubUniversidadNacional.PNG" alt="Pumas Logo" class="team-logo">
+                            <span class="team-name">Pumas</span>
+                        </div>
+                        <span class="vs">vs</span>
+                        <div class="team">
+                            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/Guadalajara_CD.svg/800px-Guadalajara_CD.svg.png" alt="Chivas Logo" class="team-logo">
+                            <span class="team-name">Chivas</span>
+                        </div>
+                        <div class="score">
+                            <span class="score-home">2</span>
+                            <span class="score-away">2</span>
+                        </div>
+                        <div class="winner draw">Empate</div>
+                    </div>
+
+                    <div class="match-card">
+                        <div class="team">
+                            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/Club_Am%C3%A9rica_crest.svg/1200px-Club_Am%C3%A9rica_crest.svg.png" alt="America Logo" class="team-logo">
+                            <span class="team-name">America</span>
+                        </div>
+                        <span class="vs">vs</span>
+                        <div class="team">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/0/09/Tigres.png" alt="Tigres Logo" class="team-logo">
+                            <span class="team-name">Tigres</span>
+                        </div>
+                        <div class="score">
+                            <span class="score-home">6</span>
+                            <span class="score-away">4</span>
+                        </div>
+                        <div class="winner">GANADOR America</div>
+                    </div>
+                </div>
+            </section>
+        </main>
     </div>
 
-    <main class="main-content">
-        <section class="results-date-range">
-            Resultados: Dom 11 - Sab 17 Mayo
-        </section>
-
-        <section class="match-day">
-            <h2>Lunes 12</h2>
-            <div class="match-cards-container">
-                <div class="match-card">
-                    <div class="team">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Club_de_F%C3%BAtbol_Monterrey_2019_Logo.svg/800px-Club_de_F%C3%BAtbol_Monterrey_2019_Logo.svg.png" alt="Monterrey Logo" class="team-logo">
-                        <span class="team-name">Monterrey</span>
+    <div class="wearable hidden bg-[#2f5a1e] font-montserrat">
+        <div class="mx-auto p-1">
+            <header class="flex items-center mb-1">
+                <a href="../index.php">
+                    <i class="fas fa-arrow-left text-base text-white"> </i>
+                </a>
+                <img class="ml-auto" height="15" src="../assets/felpudo.png" width="40" alt="Felpudo Logo" />
+            </header>
+            <p class="text-xxs mb-2 text-white">Resultados: Dom 11 - Sab 17 Mayo</p>
+            <div class="space-y-1">
+                <div class="bg-[#3f7a2a] rounded-md p-1 flex flex-col gap-0.5">
+                    <div class="flex justify-between items-center text-white text-xxs font-semibold">
+                        <span> Lunes 12 </span>
+                        <span> GANADOR </span>
                     </div>
-                    <span class="vs">vs</span>
-                    <div class="team">
-                        <img src="https://upload.wikimedia.org/wikipedia/en/5/56/Esteghlal_FC_Official_Logo.png" alt="Irán Logo" class="team-logo">
-                        <span class="team-name">Irán</span>
+                    <div class="flex justify-between items-center text-white font-semibold">
+                        <div class="flex items-center gap-0.5">
+                            <img class="w-4 h-4" height="16" src="https://storage.googleapis.com/a1aa/image/7ac6cfe6-aa23-497b-d17f-28cc4c2444cb.jpg" width="16" />
+                            <span class="text-xxs"> Monterrey </span>
+                        </div>
+                        <span class="text-lg"> 2 </span> <span class="text-sm"> vs </span> <span class="text-lg"> 0 </span>
+                        <div class="flex items-center gap-0.5">
+                            <img alt="Irán team logo" class="w-4 h-4" height="16" src="https://storage.googleapis.com/a1aa/image/992055d2-ff1e-4034-8fe9-96c0c7a4c01d.jpg" width="16" />
+                            <span class="text-xxs"> Irán </span>
+                        </div>
                     </div>
-                    <div class="score">
-                        <span class="score-home">2</span>
-                        <span class="score-away">0</span>
+                </div>
+                <div class="bg-[#3f7a2a] rounded-md p-1 flex flex-col gap-0.5">
+                    <div class="flex justify-between items-center text-white text-xxs font-semibold">
+                        <span> Miércoles 14 </span>
+                        <span> GANADOR </span>
                     </div>
-                    <div class="winner">GANADOR Monterrey</div>
+                    <div class="flex justify-between items-center text-white font-semibold">
+                        <div class="flex items-center gap-0.5">
+                            <img class="w-4 h-4" height="16" src="https://storage.googleapis.com/a1aa/image/bdddb115-644c-47d1-7b42-2ff15916c086.jpg" width="16" />
+                            <span class="text-xxs"> Manchester </span>
+                        </div>
+                        <span class="text-lg"> 1 </span>
+                        <span class="text-sm"> vs </span>
+                        <span class="text-lg"> 4 </span>
+                        <div class="flex items-center gap-0.5">
+                            <img class="w-4 h-4" height="16" src="https://storage.googleapis.com/a1aa/image/8adef8a8-d765-48ee-30d9-711fe7f43e44.jpg" width="16" alt="Chivas team logo" />
+                            <span class="text-xxs"> Chivas </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-[#3f7a2a] rounded-md p-1 flex flex-col gap-0.5">
+                    <div class="flex justify-between items-center text-white text-xxs font-semibold">
+                        <span> Miércoles 14 </span>
+                        <span> EMPATE </span>
+                    </div>
+                    <div class="flex justify-between items-center text-white font-semibold">
+                        <div class="flex items-center gap-0.5">
+                            <img class="w-4 h-4" height="16" src="https://upload.wikimedia.org/wikipedia/en/b/ba/ClubUniversidadNacional.PNG" width="16" alt="Pumas team logo" />
+                            <span class="text-xxs"> Pumas </span>
+                        </div>
+                        <span class="text-lg"> 2 </span>
+                        <span class="text-sm"> vs </span>
+                        <span class="text-lg"> 2 </span>
+                        <div class="flex items-center gap-0.5">
+                            <img class="w-4 h-4" height="16" src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/Guadalajara_CD.svg/800px-Guadalajara_CD.svg.png" width="16" alt="Chivas team logo" />
+                            <span class="text-xxs"> Chivas </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-[#3f7a2a] rounded-md p-1 flex flex-col gap-0.5">
+                    <div class="flex justify-between items-center text-white text-xxs font-semibold">
+                        <span> Miércoles 14 </span>
+                        <span> GANADOR </span>
+                    </div>
+                    <div class="flex justify-between items-center text-white font-semibold">
+                        <div class="flex items-center gap-0.5">
+                            <img class="w-4 h-4" height="16" src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/Club_Am%C3%A9rica_crest.svg/1200px-Club_Am%C3%A9rica_crest.svg.png" width="16" alt="America team logo" />
+                            <span class="text-xxs"> America </span>
+                        </div>
+                        <span class="text-lg"> 6 </span>
+                        <span class="text-sm"> vs </span>
+                        <span class="text-lg"> 4 </span>
+                        <div class="flex items-center gap-0.5">
+                            <img class="w-4 h-4" height="16" src="https://upload.wikimedia.org/wikipedia/commons/0/09/Tigres.png" width="16" alt="Tigres team logo" />
+                            <span class="text-xxs"> Tigres </span>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </div>
 
-        <section class="match-day">
-            <h2>Miercoles 14</h2>
-            <div class="match-cards-container">
-                <div class="match-card">
-                    <div class="team">
-                        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png" alt="Manchester Logo" class="team-logo">
-                        <span class="team-name">Manchester</span>
-                    </div>
-                    <span class="vs">vs</span>
-                    <div class="team">
-                        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/Guadalajara_CD.svg/800px-Guadalajara_CD.svg.png" alt="Chivas Logo" class="team-logo">
-                        <span class="team-name">Chivas</span>
-                    </div>
-                    <div class="score">
-                        <span class="score-home">1</span>
-                        <span class="score-away">4</span>
-                    </div>
-                    <div class="winner">GANADOR Chivas</div>
-                </div>
-
-                <div class="match-card">
-                    <div class="team">
-                        <img src="https://upload.wikimedia.org/wikipedia/en/b/ba/ClubUniversidadNacional.PNG" alt="Pumas Logo" class="team-logo">
-                        <span class="team-name">Pumas</span>
-                    </div>
-                    <span class="vs">vs</span>
-                    <div class="team">
-                        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/Guadalajara_CD.svg/800px-Guadalajara_CD.svg.png" alt="Chivas Logo" class="team-logo">
-                        <span class="team-name">Chivas</span>
-                    </div>
-                    <div class="score">
-                        <span class="score-home">2</span>
-                        <span class="score-away">2</span>
-                    </div>
-                    <div class="winner draw">Empate</div>
-                </div>
-
-                <div class="match-card">
-                    <div class="team">
-                        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/Club_Am%C3%A9rica_crest.svg/1200px-Club_Am%C3%A9rica_crest.svg.png" alt="America Logo" class="team-logo">
-                        <span class="team-name">America</span>
-                    </div>
-                    <span class="vs">vs</span>
-                    <div class="team">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/0/09/Tigres.png" alt="Tigres Logo" class="team-logo">
-                        <span class="team-name">Tigres</span>
-                    </div>
-                    <div class="score">
-                        <span class="score-home">6</span>
-                        <span class="score-away">4</span>
-                    </div>
-                    <div class="winner">GANADOR America</div>
-                </div>
-            </div>
-        </section>
-    </main>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            // Uncomment this if Tailwind's default base styles cause unwanted conflicts with your existing CSS.
+            // corePlugins: {
+            //   preflight: false,
+            // },
+            theme: {
+                extend: {
+                    fontFamily: {
+                        montserrat: ['Montserrat', 'sans-serif'],
+                        // You can add more custom fonts here if needed
+                    },
+                    fontSize: { // Custom font sizes for very small screens
+                        'xxs': '0.6rem', // Approx 9.6px
+                    },
+                    spacing: { // Custom spacing values for very small screens
+                        '0.5': '0.125rem', // 2px
+                        '1': '0.25rem', // 4px (default for p-1, etc.)
+                        '2': '0.5rem', // 8px (default for p-2, etc.)
+                    },
+                },
+            },
+        }
+    </script>
 </body>
 
 </html>
